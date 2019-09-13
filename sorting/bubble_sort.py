9b884001aab300475c25b50fe13e2e15f6511fa4
Ramarao Amara <34347368+ramamca90@users.noreply.github.com>
2019-09-13 22:06:49 +0530

    Update bubble_sort.py

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,7 +1,11 @@
 def bubble_sort(arr):
     
-    for i in range(len(arr)):        
+    for i in range(len(arr)):
+        # Last i elements are already in place 
         for j in range(len(arr)-i-1):
+            # traverse the array from 0 to n-i-1 
+            # Swap if the element found is greater 
+            # than the next element
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
