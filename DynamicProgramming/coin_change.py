6be7166ddc44470d50efa4744b11a3d98384076b
Ramarao Amara <34347368+ramamca90@users.noreply.github.com>
2019-09-25 08:24:38 +0530

    Update coin_change.py

--- DynamicProgramming/coin_change.py
+++ DynamicProgramming/coin_change.py
@@ -7,3 +7,18 @@ For example, for N = 4 and S = {1,2,3}, there are four solutions: {1,1,1,1},{1,1
              For N = 10 and S = {2, 5, 3, 6}, there are five solutions: {2,2,2,2,2}, {2,2,3,3}, {2,2,6}, {2,3,5} and {5,5}. 
              So the output should be 5.
 '''
+
+
+def no_ways_make_change(total_value, coins):
+    table = [0]* (total_value + 1)
+    table[0] = 1
+    for coin in range(len(coins)):
+        print(table)
+        for value in range(coins[coin], total_value + 1):
+            table[value] += table[value-coins[coin]]
+    print(table)
+    return table[-1]
+  
+value = 5
+coins = [1, 2, 3]
+no_ways_make_change(value, coins)  
