290a9762398cd9726bf70ed462c59aba29b11e90
Ramarao Amara <34347368+ramamca90@users.noreply.github.com>
2019-11-30 07:23:32 +0530

    Update insertion_sort.py

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -1,3 +1,12 @@
+'''
+Insertion sort is a simple sorting algorithm that works the way we sort playing cards in our hands.
+Algorithm
+// Sort an arr[] of size n
+insertionSort(arr, n)
+Loop from i = 1 to n-1.
+……a) Pick element arr[i] and insert it into sorted sequence arr[0…i-1]
+'''
+
 def insertion_sort(arr):
     for i in range(1, len(arr)):
         key = arr[i]
