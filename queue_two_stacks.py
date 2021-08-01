47db8b4615d864636def6438745ba999663f947c
Ramarao Amara <34347368+ramamca90@users.noreply.github.com>
2021-08-01 13:27:47 +0530

    Update queue_two_stacks.py

--- queue_two_stacks.py
+++ queue_two_stacks.py
@@ -6,41 +6,45 @@
 
 class Queue():
     def __init__(self):
-        self.stack1 = []
-        self.stack2 = []
+        self.stack1 = [] #Using for enqueue
+        self.stack2 = [] #Using for dequeue
 
     def enqueue(self, item):
         self.stack1.append(item)
         print("Item {} added to stack".format(item))
 
     def dequeue(self):
-        if self.stack1 == [] and self.stack2 == []:
+        if not self.stack1 and not self.stack2:
             print("Queue is empty, no scope for deletion")
             return
-
+        
+        #if elements present in stack2
+        if self.stack2:
+            deleted = self.stack2.pop()
+            print(f"Item {deleted} removed from queue")
+            return
+        
+        #if there is no elements in stack2
         while self.stack1:
             self.stack2.append(self.stack1.pop())
-        # if not len(self.stack2):    # stack 2 empty
-        #     self.stack2 = self.stack1[::-1]  # stack 2 =  reverse of stack 1
-        #     self.stack1 = []
-
+            
         deleted = self.stack2.pop()
         print(f"Item {deleted} removed from queue")
 
 
     def __str__(self):
-        if self.stack1 == [] and self.stack2 == []:
+        if not self.stack1 and not self.stack2:
             return "Queue empty"
 
-        if self.stack2 != [] and self.stack1 != []:
+        if self.stack2 and self.stack1:
             final = self.stack2[::-1]
             final.extend(self.stack1)
             return "{}".format(final)
 
-        if self.stack2 != []:
+        if self.stack2:
             return "{}".format(self.stack2[::-1])
 
-        if self.stack1 != []:
+        if self.stack1:
             return "{}".format(self.stack1)
 
 
